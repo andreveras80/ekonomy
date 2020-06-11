7ef3ffd85c250a1281bdac92a0b12ed3c332569b
andreveras80 <andredv@gmail.com>
2020-06-11 17:26:48 -0300

    Update gdp_wb.py

--- scripts/gdp_wb.py
+++ scripts/gdp_wb.py
@@ -2,6 +2,9 @@ from pandas_datareader import wb
 import psycopg2
 import os
 
+from sqlalchemy import create_engine
+engine = create_engine('postgresql://scott:tiger@localhost:5432/mydatabase')
+
 DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
@@ -11,4 +14,4 @@ df.columns = ['gdp']
 print(df)
 
 #df.to_csv('../gdp_wb.csv', index = True, header=True)
-df.to_sql('tb_gdp', conn)
+df.to_sql('tb_gdp', engine)
