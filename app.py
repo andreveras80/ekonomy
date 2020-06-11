8ee26163c444391088b6ae1e4da4202357c3dc17
andreveras80 <andredv@gmail.com>
2020-06-11 15:11:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
+import os
+import psycopg2
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+DATABASE_URL = os.environ['DATABASE_URL']
+conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 @app.route("/")
 def home():
