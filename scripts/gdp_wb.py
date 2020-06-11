5f4aa2843f130f26a54e55e38267068fcbcb8963
andreveras80 <andredv@gmail.com>
2020-06-11 14:38:37 -0300

    Update gdp_wb.py

--- scripts/gdp_wb.py
+++ scripts/gdp_wb.py
@@ -2,6 +2,6 @@ from pandas_datareader import wb
 
 df = wb.download(indicator='NY.GDP.PCAP.KD', country=['US', 'BR'], start=2005, end=2008)
 
-#print(df)
+print(df)
 
 df.to_csv('../gdp_wb.csv', index = True, header=True)
