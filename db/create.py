7d8d1f7fac7306f36e5234a1408583b5d21b522a
andreveras80 <andredv@gmail.com>
2020-06-11 16:37:19 -0300

    Update create.py

--- db/create.py
+++ db/create.py
@@ -1,9 +1,32 @@
+import psycopg2
+from psycopg2 import Error
 
+try:
 
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+    DATABASE_URL = os.environ['DATABASE_URL']
+    connection = psycopg2.connect(DATABASE_URL, sslmode='require')
 
-cursor = conn.cursor()
-sql = "create table tb_gdp (country varchar(128), year integer, gdp real);"
-cursor.execute(sql)
-conn.commit()
+#    connection = psycopg2.connect(user = "postgres",
+#                                  password = "pass@#29",
+#                                  host = "127.0.0.1",
+#                                  port = "5432",
+#                                  database = "postgres_db")
+
+    cursor = connection.cursor()
+    
+    create_table_query = '''CREATE TABLE tb_gdp
+          (DATE date PRIMARY KEY     NOT NULL,
+          VALUE           REAL    NOT NULL); '''
+    
+    cursor.execute(create_table_query)
+    connection.commit()
+    print("Table created successfully in PostgreSQL ")
+
+except (Exception, psycopg2.DatabaseError) as error :
+    print ("Error while creating PostgreSQL table", error)
+finally:
+    #closing database connection.
+        if(connection):
+            cursor.close()
+            connection.close()
+            print("PostgreSQL connection is closed")
