0142fc471d2d607cb59b73589ccdce385feabb22
andreveras80 <andredv@gmail.com>
2020-06-11 15:54:20 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 cursor = conn.cursor()
-sql = "create table tb_gdp (country varchar(128), year int, gdp float);"
+sql = "create table tb_gdp (country varchar(128), year integer, gdp real);"
 cursor.execute(sql)
 conn.commit()
 
