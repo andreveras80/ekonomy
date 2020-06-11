84f1938fbbc67631e250e4496ffedaf925202809
andreveras80 <andredv@gmail.com>
2020-06-11 18:31:34 -0300

    Update gdp_wb.py

--- scripts/gdp_wb.py
+++ scripts/gdp_wb.py
@@ -3,7 +3,7 @@ import psycopg2
 import os
 
 from sqlalchemy import create_engine
-engine = create_engine('postgres://nmkeqfzylbgsyx:54bd78f6ee3957e7765b5cd8fcfaf21651f165a7cab99b06fa92754dfdb684ac@ec2-34-202-88-122.compute-1.amazonaws.com:5432/dcnr29vkhe97fv')
+engine = create_engine('postgres://dbvbnsaobxgadt:ecf6895ee7eccdd930382020298a116479bc44eeb8a0090992fa638b3418d6a7@ec2-52-202-146-43.compute-1.amazonaws.com:5432/d35346em5lnoul')
 
 df = wb.download(indicator='NY.GDP.PCAP.KD', country=['US', 'BR'], start=2005, end=2008)
 df.columns = ['gdp']
