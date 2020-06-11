2545c7f6b4d5b17d6990a7df44b99d67c24cb2ae
andreveras80 <andredv@gmail.com>
2020-06-11 16:27:17 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,6 @@ import psycopg2
 from flask import Flask, render_template
 
 app = Flask(__name__)
-
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
-
-cursor = conn.cursor()
-sql = "create table tb_gdp (country varchar(128), year integer, gdp real);"
-cursor.execute(sql)
-conn.commit()
-
 @app.route("/")
 def home():
     return render_template("home.html")
