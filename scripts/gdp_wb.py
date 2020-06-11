d23011f993b770cd55254fb6580bf40308518501
andreveras80 <andredv@gmail.com>
2020-06-11 18:36:11 -0300

    Update gdp_wb.py

--- scripts/gdp_wb.py
+++ scripts/gdp_wb.py
@@ -4,10 +4,11 @@ import pandas_datareader.data as web
 from sqlalchemy import create_engine
 engine = create_engine('postgres://dbvbnsaobxgadt:ecf6895ee7eccdd930382020298a116479bc44eeb8a0090992fa638b3418d6a7@ec2-52-202-146-43.compute-1.amazonaws.com:5432/d35346em5lnoul')
 
-df = web.DataReader(['CPILFESL'], 'fred', start, end)
+start = datetime.datetime(2010, 1, 1)
 
-df = wb.download(indicator='NY.GDP.PCAP.KD', country=['US', 'BR'], start=2005, end=2008)
-df.columns = ['gdp']
+end = datetime.datetime(2013, 1, 27)
+
+df = web.DataReader(['CPILFESL'], 'fred', start, end)
 
 print(df)
 
