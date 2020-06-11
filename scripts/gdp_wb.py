fafd5aef676fba9d4dd74324088af8b14851e5df
andreveras80 <andredv@gmail.com>
2020-06-11 17:30:41 -0300

    Update gdp_wb.py

--- scripts/gdp_wb.py
+++ scripts/gdp_wb.py
@@ -3,10 +3,7 @@ import psycopg2
 import os
 
 from sqlalchemy import create_engine
-engine = create_engine('postgresql://scott:tiger@localhost:5432/mydatabase')
-
-DATABASE_URL = os.environ['DATABASE_URL']
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+engine = create_engine('postgres://nmkeqfzylbgsyx:54bd78f6ee3957e7765b5cd8fcfaf21651f165a7cab99b06fa92754dfdb684ac@ec2-34-202-88-122.compute-1.amazonaws.com:5432/dcnr29vkhe97fv')
 
 df = wb.download(indicator='NY.GDP.PCAP.KD', country=['US', 'BR'], start=2005, end=2008)
 df.columns = ['gdp']
