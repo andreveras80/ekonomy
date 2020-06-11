8abe8fcb46cf692566b110f1dc90b94ab254ca8f
andreveras80 <andredv@gmail.com>
2020-06-11 18:35:17 -0300

    Update gdp_wb.py

--- scripts/gdp_wb.py
+++ scripts/gdp_wb.py
@@ -1,9 +1,10 @@
 from pandas_datareader import wb
-import psycopg2
-import os
+import pandas_datareader.data as web
 
 from sqlalchemy import create_engine
 engine = create_engine('postgres://dbvbnsaobxgadt:ecf6895ee7eccdd930382020298a116479bc44eeb8a0090992fa638b3418d6a7@ec2-52-202-146-43.compute-1.amazonaws.com:5432/d35346em5lnoul')
+
+df = web.DataReader(['CPILFESL'], 'fred', start, end)
 
 df = wb.download(indicator='NY.GDP.PCAP.KD', country=['US', 'BR'], start=2005, end=2008)
 df.columns = ['gdp']
