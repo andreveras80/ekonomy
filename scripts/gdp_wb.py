6bf4f4a7471a42e29a8efed604ea422d496de92f
andreveras80 <andredv@gmail.com>
2020-06-11 15:50:43 -0300

    Update gdp_wb.py

--- scripts/gdp_wb.py
+++ scripts/gdp_wb.py
@@ -6,8 +6,9 @@ DATABASE_URL = os.environ['DATABASE_URL']
 conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 df = wb.download(indicator='NY.GDP.PCAP.KD', country=['US', 'BR'], start=2005, end=2008)
+df.columns = ['gdp']
 
 print(df)
 
 #df.to_csv('../gdp_wb.csv', index = True, header=True)
-df.to_sql('table_name', conn)
+df.to_sql('tb_gdp', conn)
